69ecc6f7c572fbacaef5b58997d5b899b3efe180
juhyun10 <juhyun1010@naver.com>
2019-04-01 15:33:37 +0900

    lesson01. binaryGap

--- src/main/java/lesson01/BinaryGap.java
+++ src/main/java/lesson01/BinaryGap.java
@@ -7,25 +7,33 @@ public class BinaryGap {
     public static void main(String[] args) {
         int N = 15;
         String biStr = Integer.toBinaryString(N);
-        int biStrSize = 0;
-        int biStrSize1 = 0;
-        Pattern p = Pattern.compile("1");
-        Pattern p1 = Pattern.compile("0");
-        Matcher m = p.matcher(biStr);
-        Matcher m1 = p1.matcher(biStr);
-        while (m.find()) {
-            biStrSize++;
-        }
-        while (m1.find()) {
-            biStrSize1++;
+
+        int biOneLen = 0;
+        int biZeroLen = 0;
+
+        Pattern oneP = Pattern.compile("1");
+        Pattern zeroP = Pattern.compile("0");
+        Matcher oneM = oneP.matcher(biStr);
+        Matcher zeroM = zeroP.matcher(biStr);
+
+        // 0의 갯수
+        while (oneM.find()) {
+            biOneLen++;
         }
 
-        if (biStrSize < 2 || biStrSize1 == 0) {
+        // 1의 갯수
+        while (zeroM.find()) {
+            biZeroLen++;
+        }
+
+        // 1이 1개 이하이거나 0이 없으면 "0" 리턴
+        if (biOneLen < 2 || biZeroLen == 0) {
             System.out.println("0");
         } else {
             String[] biArr = biStr.split("1");
             int biGap = 0;
-            for (int i=0; i<biStrSize; i++) {
+            // 1로 split한 배열의 수만큼 돌면서 0의 갯수 카운트
+            for (int i=0; i<biArr[i].length(); i++) {
                 if (biGap < biArr[i].length()) {
                     biGap = biArr[i].length();
                 }
