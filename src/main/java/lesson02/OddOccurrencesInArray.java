ac070a06235a3ca870275deed7b67bd711307e27
juhyun10 <juhyun1010@naver.com>
2019-04-06 22:18:07 +0900

    lesson02. OddOccurrencesInArray (Modify)

--- src/main/java/lesson02/OddOccurrencesInArray.java
+++ src/main/java/lesson02/OddOccurrencesInArray.java
@@ -1,28 +1,70 @@
 package lesson02;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.IntStream;
 
+/**
+ * 자료구조 문제, 등장 횟수 측정의 부하를 줄이는 것이 핵심 (Hash사용 추천)
+ */
 public class OddOccurrencesInArray {
 	public static void main(String[] args) {
 		
-		int[] A = {9,3,9,3,9,7,9};
-		System.out.println(solution(A));
+		int[] A = {9,3,9,3,7};
+		System.out.println(solution2(A));
 	}
 	
-	public static int solution(int[] A) {
-		// 정렬
-		//IntStream stream = IntStream.of(A).sorted();
-		int[] arrInt = IntStream.of(A).sorted().toArray();
+	public static int solution2(int[] A) {
+		Map<Integer, Integer> countMap = new HashMap<>();
 		
-		// 출력
-//		stream.forEach(i -> System.out.println(i));
+		for (int i = 0; i < A.length; i++) {
+			increaseCount(countMap, A[i]);
+		}
 		
-		for (int i = 0; i<arrInt.length; i=i+2) {
-			if (arrInt[i] != arrInt[i+1]) {
-				return arrInt[i];
+		for (Integer key : countMap.keySet()) {
+			if (countMap.get(key) % 2 != 0) {
+				return key;
 			}
 		}
 		
-		return 0;
+		return -1;
+	}
+	
+	private static void increaseCount(Map<Integer, Integer> countMap, int targetNum) {
+		Integer target = countMap.get(targetNum);
+		
+		if (target == null) {
+			countMap.put(targetNum, 1);
+		}
+		else {
+			countMap.put(targetNum, target + 1);
+		}
+	}
+	
+	
+	
+	public static int solution(int[] A) {
+		// 정렬
+//		IntStream stream = IntStream.of(A).sorted();
+//		stream.forEach(i -> System.out.println(i));
+		int rst = 0;
+		
+		if (A.length == 1) {
+			return A[0];
+		}
+		
+		int[] arrInt = IntStream.of(A).sorted().toArray();
+		
+		for (int i = 0; i<arrInt.length/2; i=i+2) {
+			if (arrInt[i] != arrInt[i+1]) {
+				rst = arrInt[i];
+			}
+		}
+		
+		if (rst == 0) {
+			rst = A[A.length-1];
+		}
+		
+		return rst;
 	}
 }
